7afb6022ba4de307fc096f8f474151d165c8ca7b
AnubhabDey-TransEV <transmogrify21@outlook.com>
2024-12-03 13:02:20 +0530

    Updates

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -9,8 +9,7 @@ import { JwtModule } from '@nestjs/jwt'; // Import JwtModule here
   imports: [
     JwtModule.register({
       // Configure JwtModule (optional, but good for custom settings)
-      secret:
-        '8gpetQzbkhUJSmqs5X4YF4MfVr8dOaf-liq5aiSYfCk5TZFoQQJ5tzy-UIIFl9x47Fo1pEpReChmRj5hbidh_GRLW4bNd0o9OokN-JopyHAXnqCcRRo_xftX1wP0Bkj9oqbv_IYGNZM0umDOGS_YQNsm9Vxo5SVwsl9tgattpkFpHI1KeuEPePferuz9JGRxrEGrOJsJIiCk1v_5uw_NHEbUGV_Q0jnDeQ6m-h69RnKEPM02MMc8GG75_oJ_9uXdIwozq78b-lcqtua_08AyiZt4ND-lOjwitV8j4w0trJ36Axf-XWK0hVpmE1D8MLxW9DYqB1vkrcmfYXUuaKAr', // Replace with your own secret key
+      secret: process.env.JWT_SECRET,
       signOptions: { expiresIn: '24h' }, // Example setting for token expiration
     }),
   ],
