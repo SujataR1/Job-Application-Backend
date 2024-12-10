700d6ae6ae973baa46cd1d2a662f2bc38d66ad6f
AnubhabDey-TransEV <transmogrify21@outlook.com>
2024-12-10 16:53:05 +0530

    Updated stuff

---
+++ prisma/migrations/20241210111219_corrrected_names/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `commentId` on the `comment_attachments` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE `comment_attachments` DROP COLUMN `commentId`;
