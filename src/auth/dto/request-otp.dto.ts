be738fb7b931a5ee4a6240d6cc9388b1dfc9e763
AnubhabDey-TransEV <transmogrify21@outlook.com>
2024-12-10 12:39:58 +0530

    Updated stuff

---
+++ src/auth/dto/request-otp.dto.ts
@@ -0,0 +1,22 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsString, IsNotEmpty, IsEmail, IsEnum } from 'class-validator';
+import { OTPType } from '@prisma/client';
+
+export class RequestOtpDto {
+  @ApiProperty({
+    description: 'The email associated with the account',
+    example: 'user@example.com',
+  })
+  @IsEmail()
+  @IsNotEmpty()
+  email: string;
+
+  @ApiProperty({
+    description: 'The type of OTP requested',
+    enum: OTPType,
+    example: OTPType.EmailVerification,
+  })
+  @IsEnum(OTPType)
+  @IsNotEmpty()
+  otpType: OTPType;
+}
