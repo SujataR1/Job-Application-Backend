3f4d98acca015cd1df404bd340851006087d0483
SayanChatterjee-TransEv <transmogrify08@outlook.com>
2024-12-18 14:31:17 +0530

    notification

--- src/notifications/notification.module.ts
+++ src/notifications/notification.module.ts
@@ -2,7 +2,7 @@ import { Module, forwardRef } from '@nestjs/common';
 import { NotificationService } from './notification.service';
 import { NotificationGateway } from './notification.gateway';
 import { PrismaService } from '../prisma.service';
-import { NotificationController } from './notification.controller';
+
 
 @Module({
   providers: [NotificationService, NotificationGateway, PrismaService],
