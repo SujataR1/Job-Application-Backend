0a94a7739e77083549f4a71c9e0cdbdb5ed21dec
AnubhabDey-TransEV <transmogrify21@outlook.com>
2025-01-08 11:40:29 +0530

    Updates

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,3 @@
-// import { Module } from '@nestjs/common';
-// import { AppController } from './app.controller';
-// import { AppService } from './app.service';
-// import { AuthModule } from './auth/auth.module';
-
-// @Module({
-//   imports: [AuthModule],
-//   controllers: [AppController],
-//   providers: [AppService],
-// })
-// export class AppModule {}
-
-// src/app.module.ts
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { PrismaModule } from 'prisma/prisma.module';
